7c529caa68210d690d3f3204a7dcadf20d0880b8
q12we34rt5 <luke123132112@gmail.com>
2024-05-22 22:33:15 +0800

    improve error message clarity

--- sgf_tool/exceptions.py
+++ sgf_tool/exceptions.py
@@ -1,20 +1,13 @@
-class LexicalError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
-
-
-class SGFError(Exception):
-    def __init__(self, message, start, end, detail=False, sgf=None, offset=20, highlight='\033[1;31m'):
+class BaseSGFException(Exception):
+    def __init__(self, message, start, end, detail=False, sgf=None, offset=20, highlight_start='\033[1;31m', highlight_end='\033[0m'):
         self.message = message
         self.start = start
         self.end = end
         self.detail = detail
         self.sgf = sgf
         self.offset = offset
-        self.highlight = highlight
+        self.highlight_start = highlight_start
+        self.highlight_end = highlight_end
 
     def __str__(self):
         if not self.detail:
@@ -24,4 +17,17 @@ class SGFError(Exception):
                 return f'{self.message} at {self.start}:{self.end}'
             s = max(0, self.start - self.offset)
             e = min(len(self.sgf), self.end + self.offset)
-            return f'{self.message} at {self.start}:{self.end}\n{self.sgf[s:self.start]}{self.highlight}{self.sgf[self.start:self.end]}\033[0m{self.sgf[self.end:e]}'
+            return (
+                f'{self.message} at {self.start}:{self.end}\n'
+                f'{self.sgf[s:self.start]}{self.highlight_start}'
+                f'{self.sgf[self.start:self.end]}{self.highlight_end}'
+                f'{self.sgf[self.end:e]}'
+            )
+
+
+class LexicalError(BaseSGFException):
+    pass
+
+
+class SGFError(BaseSGFException):
+    pass
